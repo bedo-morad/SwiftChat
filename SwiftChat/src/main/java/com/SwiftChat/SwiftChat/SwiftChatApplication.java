dd21a2d37ef24e6ee43ace6ead5b0bfa5088eced
Abdalrahman Mosad <bodymosad67@gmail.com>
2026-07-21 20:23:54 +0300

    init

---
+++ SwiftChat/src/main/java/com/SwiftChat/SwiftChat/SwiftChatApplication.java
@@ -0,0 +1,13 @@
+package com.SwiftChat.SwiftChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftChatApplication.class, args);
+	}
+
+}
---
+++ SwiftChat/src/test/java/com/SwiftChat/SwiftChat/SwiftChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SwiftChat.SwiftChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
