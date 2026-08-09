0aba3d86d40d0083914e5a06620460649d23db2d
Abdalrahman Mosad <bodymosad67@gmail.com>
2026-08-09 16:06:01 +0300

    feat: implement Keycloak authentication with HTTP interceptor

--- SwiftChat-UI/src/app/app.config.ts
+++ SwiftChat-UI/src/app/app.config.ts
@@ -1,13 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideAppInitializer, inject} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {KeycloakService} from './utils/keycloak/keycloak-service';
+import {keycloakHttpInterceptor} from './utils/http/keycloak-http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([keycloakHttpInterceptor])
+    ),
+    provideAppInitializer(() => {
+      const initFunction = ((key: KeycloakService) => {
+        return () => key.init();
+      })(inject(KeycloakService));
+      return initFunction()
+    })
   ]
 };
---
+++ SwiftChat-UI/src/app/utils/keycloak/keycloak-service.ts
@@ -0,0 +1,51 @@
+import {Service} from '@angular/core';
+import Keycloak from 'keycloak-js';
+
+@Service()
+export class KeycloakService {
+  private _keycloak: Keycloak | undefined;
+
+  constructor() {
+  }
+
+  get keycloak() {
+    if (!this._keycloak) {
+      this._keycloak = new Keycloak({
+        url: 'http://localhost:9090',
+        realm: 'SwiftChat',
+        clientId: 'SwiftChat'
+      })
+    }
+    return this._keycloak;
+  }
+
+  async init() {
+    const authenticated = await this.keycloak.init({
+      onLoad: 'login-required'
+    });
+  }
+
+  async login() {
+    await this.keycloak.login();
+  }
+
+  get userId(): string {
+    return this.keycloak?.tokenParsed?.sub as string;
+  }
+
+  get isTokenValid() {
+    return !this.keycloak.isTokenExpired();
+  }
+
+  get fullName(): string {
+    return this.keycloak?.tokenParsed?.['name'] as string;
+  }
+
+  logout() {
+    return this.keycloak.logout({redirectUri: 'http://localhost:4200'});
+  }
+
+  accountManagement() {
+    return this.keycloak.accountManagement();
+  }
+}
